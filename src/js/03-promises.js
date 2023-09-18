770c6a20aa5a0950bfe38a7f4980089761747b3a
PavloDereniuk <125708520+PavloDereniuk@users.noreply.github.com>
2023-09-18 22:55:46 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector("form");
+
+form.addEventListener('submit', onSubmit);
+
+let delay = 0;
+
+function onSubmit(event) {
+  event.preventDefault();
+  let delayStart = parseInt(form.delay.value);
+  let position = parseInt(form.amount.value);
+  let step = parseInt(form.step.value); 
+
+  for (let i = 1; i <= position; i += 1) {
+    delay = delayStart + step * (i - 1);
+    createPromise (i, delay)
+      .then((message) => {
+        Notiflix.Notify.success(message);
+      })
+      .catch((message) => {
+        Notiflix.Notify.failure(message);
+      });
+}}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        } else {
+          reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        }
+      }, delay);
+      
+    });
+    
+  };
